1cb5f75f6d2b761de6a963566bc2fc4236633595
nishadd1402 <102455135+nishadd1402@users.noreply.github.com>
2022-06-26 11:24:54 +0530

    Time: 1 ms (93.68%), Space: 42.4 MB (40.59%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.java
+++ 844-backspace-string-compare/844-backspace-string-compare.java
@@ -1,5 +1,49 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
+        int i = s.length() - 1;
+        int j = t.length() - 1;
+        while (i >= 0 || j >= 0) {
+            if ((i >= 0 && s.charAt(i) == '#') || (j >= 0 && t.charAt(j) == '#')) {
+                if (i >= 0 && s.charAt(i) == '#') { 
+                    int backCount = 2;
+
+                    while (backCount > 0) {
+                        i--;
+                        backCount--;
+                        if (i >= 0 && s.charAt(i) == '#') {
+                            backCount = backCount + 2;
+                        }
+                    }
+                }
+
+                if (j >= 0 && t.charAt(j) == '#') {
+                    int backCount = 2;
+
+                    while (backCount > 0) {
+                        j--;
+                        backCount--;
+                        if (j >= 0 && t.charAt(j) == '#') {
+                            backCount = backCount + 2;
+                        }
+                    }
+                }
+
+            } else {
+                if ((i < 0 || j < 0) || (s.charAt(i) != t.charAt(j))) {
+                    return false;
+                } else {
+                    i--;
+                    j--;
+                }
+            }
+        }
+
+        return true;
+
+    }
+    
+    /*
+    public boolean backspaceCompare(String s, String t) {        
         Solution l = new Solution();
         List<Character> sFinal = l.finalString(s);
         List<Character> tFinal = l.finalString(t);
@@ -31,5 +75,5 @@ class Solution {
         }
 
         return strChars;
-    }
+    }*/
 }
